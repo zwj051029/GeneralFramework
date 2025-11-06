4157637490b5a07292093d9598f27d699b63d162
Huangney <1260371422@qq.com>
2025-11-06 21:06:25 +0800

    完成Bsp_tim_pwm文件

--- Libs/inc/bsp_tim_pwm.h
+++ Libs/inc/bsp_tim_pwm.h
@@ -1,9 +1,25 @@
 #ifndef BSP_TIM_PWM_H
 #define BSP_TIM_PWM_H
 
+#include "stm32f4xx_hal.h"
+
+typedef struct
+{
+    TIM_HandleTypeDef *htim;        // PWM定时器句柄
+    uint32_t channel;               // PWM通道
+    uint8_t enabled;                // PWM使能标志
+
+    uint32_t auto_reload_value;     // 自动重装载寄存器的值（ARR）
+    uint32_t compare_value;         // 比较寄存器的值（CCR）
+
+    float duty;                     // PWM占空比
+    float (*GetFreq)();             // 获取PWM频率的函数指针    
+}BspTIMPWM_TypeDef;
 
 
+void BspTIMPWM_InstRegist(BspTIMPWM_TypeDef *tim_inst, TIM_HandleTypeDef *htim, uint32_t channel);
 
+void BspTIMPWM_SetDuty(BspTIMPWM_TypeDef *tim_inst, float duty);
 
 
 #endif
--- Libs/src/bsp_tim_pwm.c
+++ Libs/src/bsp_tim_pwm.c
@@ -1,1 +1,37 @@
 #include "bsp_tim_pwm.h"
+
+void BspTIMPWM_InstRegist(BspTIMPWM_TypeDef *tim_inst, TIM_HandleTypeDef *htim, uint32_t channel)
+{
+    tim_inst->htim = htim;
+    tim_inst->channel = channel;
+
+    // 获取ARR的值
+    tim_inst->auto_reload_value = __HAL_TIM_GET_AUTORELOAD(tim_inst->htim);
+    // 获取CCR的值
+    tim_inst->compare_value = __HAL_TIM_GET_COMPARE(tim_inst->htim, tim_inst->channel);
+
+    // 设置初始占空比为 0
+    BspTIMPWM_SetDuty(tim_inst, 0.0f);
+}
+
+
+void BspTIMPWM_SetDuty(BspTIMPWM_TypeDef *tim_inst, float duty)
+{
+    if(duty < 0.0f) duty = 0.0f;
+    if(duty > 1.0f) duty = 1.0f;
+
+    tim_inst->duty = duty;
+    
+    // 计算CCR的对应值
+    tim_inst->compare_value = (uint32_t)(tim_inst->auto_reload_value * duty);
+    __HAL_TIM_SET_COMPARE(tim_inst->htim, tim_inst->channel, tim_inst->compare_value);
+}
+
+
+void BspTIMPWM_Enable(BspTIMPWM_TypeDef *tim_inst)
+{
+    HAL_TIM_PWM_Start(tim_inst->htim, tim_inst->channel);
+    tim_inst->enabled = 1;
+}
+
+
